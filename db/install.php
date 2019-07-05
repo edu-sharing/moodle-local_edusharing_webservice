3a5f476050373a2322531d54f1e68f83353bd5ec
shippeli <hippeli@edu-sharing.com>
2019-07-05 16:16:04 +0200

    add postgresql support

--- db/install.php
+++ db/install.php
@@ -5,9 +5,17 @@ defined('MOODLE_INTERNAL') || die();
 
 function xmldb_local_edusharing_install(){
     global $DB;
-    $query = 'ALTER TABLE ' . $DB->get_prefix() . 'scorm MODIFY COLUMN reference VARCHAR(1000)';
-    $DB->execute($query);
-    $query = 'ALTER TABLE ' . $DB->get_prefix() . 'files MODIFY COLUMN filename VARCHAR(1000)';
-    $DB->execute($query);
+    $dbFamily = $DB->get_dbfamily();
+    if($dbFamily === 'mysql') {
+        $query = 'ALTER TABLE ' . $DB->get_prefix() . 'scorm MODIFY COLUMN reference VARCHAR(1000)';
+        $DB->execute($query);
+        $query = 'ALTER TABLE ' . $DB->get_prefix() . 'files MODIFY COLUMN filename VARCHAR(1000)';
+        $DB->execute($query);
+    } else if($dbFamily === 'postgres') {
+        $query = 'ALTER TABLE ' . $DB->get_prefix() . 'scorm ALTER COLUMN reference TYPE varchar(1000)';
+        $DB->execute($query);
+        $query = 'ALTER TABLE ' . $DB->get_prefix() . 'files ALTER COLUMN filename TYPE  varchar(1000)';
+        $DB->execute($query);
+    }
     return true;
 }
--- version.php
+++ version.php
@@ -1,5 +1,5 @@
 <?php
 
 defined('MOODLE_INTERNAL') || die();
-$plugin -> version  = 2018103001;
+$plugin -> version  = 2019070501;
 $plugin -> component = 'local_edusharing';
